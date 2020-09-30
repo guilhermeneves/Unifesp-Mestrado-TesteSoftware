d31322f0b619a0eb084a35f529dffd68b662d7e7
Neves, Guilherme <guilherme.neves@monsanto.com>
2020-09-29 21:43:18 -0300

    refactor: extract game initialization to a setup method

--- S6-TDD-Kata-Bowling/src/game.spec.ts
+++ S6-TDD-Kata-Bowling/src/game.spec.ts
@@ -1,16 +1,22 @@
 import { Game } from './game'
 import { assert } from 'console'
 
+let g: Game = new Game()
+
+beforeEach(() => {
+  g = new Game()
+})
+
 describe('Bowling Game', () => {
   test('Should score 0 for gutter game', () => {
-    const g: Game = new Game()
     Array(20).fill(0).map(() => { g.roll(0) })
     expect(g.score).toBe(0)
   })
 
   test('should score 20 for all ones game', () => {
-    const g: Game = new Game()
     Array(20).fill(0).map(() => { g.roll(1) })
     expect(g.score).toBe(20)
   })
 })
+
+function _
