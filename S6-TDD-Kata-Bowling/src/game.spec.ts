d0345f196a3faab6d25d08ac421f403aa0f9fdac
Neves, Guilherme <guilherme.neves@monsanto.com>
2020-09-29 22:40:11 -0300

    test: strike score

--- S6-TDD-Kata-Bowling/src/game.spec.ts
+++ S6-TDD-Kata-Bowling/src/game.spec.ts
@@ -23,13 +23,22 @@ describe('Bowling Game', () => {
     g.roll(3)
     expect(g.score()).toBe(16)
   })
+
+  test('should score 24 for a strike followed by a 3 and 4 balls', () => {
+    g.roll(10) //strike
+    g.roll(3)
+    g.roll(4)
+    rollMany(16, 0)
+    expect(g.score()).toBe(24)
+  })
+
+  function rollSpare (): void {
+    g.roll(5)
+    g.roll(5)
+  }
+  
+  function rollMany (n: number, pins: number): void {
+    Array(n).fill(0).map(() => { g.roll(pins) })
+  }
+
 })
-
-function rollSpare (): void {
-  g.roll(5)
-  g.roll(5)
-}
-
-function rollMany (n: number, pins: number): void {
-  Array(n).fill(0).map(() => { g.roll(pins) })
-}
