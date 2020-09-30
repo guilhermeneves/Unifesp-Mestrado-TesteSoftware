ec3cf5eba9692b67c771e9f1b281c5671cc6e42e
Neves, Guilherme <guilherme.neves@monsanto.com>
2020-09-29 21:40:09 -0300

    test: all ones game

--- S6-TDD-Kata-Bowling/src/game.spec.ts
+++ S6-TDD-Kata-Bowling/src/game.spec.ts
@@ -4,7 +4,13 @@ import { assert } from 'console'
 describe('Bowling Game', () => {
   test('Should score 0 for gutter game', () => {
     const g: Game = new Game()
-    //Array(20).fill(0).map(() => { g.roll() })
-    expect(g.score()).toBe(0)
+    Array(20).fill(0).map(() => { g.roll(0) })
+    expect(g.score).toBe(0)
+  })
+
+  test('should score 20 for all ones game', () => {
+    const g: Game = new Game()
+    Array(20).fill(0).map(() => { g.roll(1) })
+    expect(g.score).toBe(20)
   })
 })
